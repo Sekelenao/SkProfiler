15884a6140e2fe904c36228417e467be8a023720
Sekelenao <Sekelenao@protonmail.com>
2025-01-26 17:05:34 +0100

    Modified the bytesAsHumanReadable method to return the precise value and the approximation

--- src/test/java/io/github/sekelenao/skprofiler/util/TestUnits.java
+++ src/test/java/io/github/sekelenao/skprofiler/util/TestUnits.java
@@ -57,11 +57,14 @@ final class TestUnits {
     @DisplayName("FormatBytes works with exact units up to GiB")
     void bytesAsHumanReadableWorksWithExactUnitsUpToGiB() {
         assertAll(
-                () -> assertEquals("1.00 B", Units.bytesAsHumanReadable(1)),
-                () -> assertEquals("1.00 KiB", Units.bytesAsHumanReadable(1024)),
-                () -> assertEquals("1.00 MiB", Units.bytesAsHumanReadable(1024 * 1024)),
-                () -> assertEquals("1.00 GiB", Units.bytesAsHumanReadable(1024L * 1024 * 1024)),
-                () -> assertEquals("1024.00 GiB", Units.bytesAsHumanReadable(1024L * 1024 * 1024 * 1024)) // 1 TiB in GiB
+                () -> assertEquals("1 bytes", Units.bytesAsHumanReadable(1)),
+                () -> assertEquals("1024 bytes (~1.00 KiB)", Units.bytesAsHumanReadable(1024)),
+                () -> assertEquals("1048576 bytes (~1.00 MiB)", Units.bytesAsHumanReadable(1024 * 1024)),
+                () -> assertEquals("1073741824 bytes (~1.00 GiB)", Units.bytesAsHumanReadable(1024L * 1024 * 1024)),
+                () -> assertEquals(
+                    "1099511627776 bytes (~1024.00 GiB)",
+                    Units.bytesAsHumanReadable(1024L * 1024 * 1024 * 1024)
+                )
         );
     }
 
@@ -69,14 +72,23 @@ final class TestUnits {
     @DisplayName("FormatBytes handles non-round values up to GiB")
     void bytesAsHumanReadableWorksWithNonRoundValuesUpToGiB() {
         assertAll(
-                () -> assertEquals("1.23 KiB", Units.bytesAsHumanReadable(1260)),
-                () -> assertEquals("12.34 KiB", Units.bytesAsHumanReadable(12634)),
-                () -> assertEquals("3.45 MiB", Units.bytesAsHumanReadable(3612672)),
-                () -> assertEquals("7.89 GiB", Units.bytesAsHumanReadable(8472498176L)),
-                () -> assertEquals("122.88 GiB", Units.bytesAsHumanReadable(131941395333L)),
-                () -> assertEquals("5918747.61 GiB", Units.bytesAsHumanReadable(6355206855698432L)),
-                () -> assertEquals("1288490188.80 GiB", Units.bytesAsHumanReadable(1383505805528216371L)),
-                () -> assertEquals("8589934592.00 GiB", Units.bytesAsHumanReadable(Long.MAX_VALUE))
+                () -> assertEquals("1260 bytes (~1.23 KiB)", Units.bytesAsHumanReadable(1260)),
+                () -> assertEquals("12634 bytes (~12.34 KiB)", Units.bytesAsHumanReadable(12634)),
+                () -> assertEquals("3612672 bytes (~3.45 MiB)", Units.bytesAsHumanReadable(3612672)),
+                () -> assertEquals("8472498176 bytes (~7.89 GiB)", Units.bytesAsHumanReadable(8472498176L)),
+                () -> assertEquals("131941395333 bytes (~122.88 GiB)", Units.bytesAsHumanReadable(131941395333L)),
+                () -> assertEquals(
+                    "6355206855698432 bytes (~5918747.61 GiB)",
+                    Units.bytesAsHumanReadable(6355206855698432L)
+                ),
+                () -> assertEquals(
+                    "1383505805528216371 bytes (~1288490188.80 GiB)",
+                    Units.bytesAsHumanReadable(1383505805528216371L)
+                ),
+                () -> assertEquals(
+                    "9223372036854775807 bytes (~8589934592.00 GiB)",
+                    Units.bytesAsHumanReadable(Long.MAX_VALUE)
+                )
         );
     }
 
