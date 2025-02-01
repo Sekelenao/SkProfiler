e3089db4b3c2266e4339f7c890ee5044c18ff0e0
Sekelenao <Sekelenao@protonmail.com>
2025-02-02 00:03:28 +0100

    Endpoints are now handling request params

--- src/main/java/io/github/sekelenao/skprofiler/http/endpoint/StatusEndpoint.java
+++ src/main/java/io/github/sekelenao/skprofiler/http/endpoint/StatusEndpoint.java
@@ -16,7 +16,7 @@ public final class StatusEndpoint implements Endpoint {
     }
 
     @Override
-    public CustomHttpResponse processGetRequest() {
+    public CustomHttpResponse processGetRequest(String requestQuery) {
         return CustomHttpResponse.success(
             new StatusDTO(
                 EnvironmentProperties.command().orElse(Optionals.missingValueDescriptor()),
--- src/test/java/io/github/sekelenao/skprofiler/http/endpoint/TestStatusEndpoint.java
+++ src/test/java/io/github/sekelenao/skprofiler/http/endpoint/TestStatusEndpoint.java
@@ -55,7 +55,7 @@ final class TestStatusEndpoint {
                             )
                         )
                     ),
-                    statusEndpoint.processGetRequest()
+                    statusEndpoint.processGetRequest("")
                 ),
                 () -> {
                     mockedStatic.when(EnvironmentProperties::command).thenReturn(Optional.empty());
@@ -73,7 +73,7 @@ final class TestStatusEndpoint {
                                     Units.durationAsHumanReadable(duration)
                                 )
                             )
-                        ), statusEndpoint.processGetRequest());
+                        ), statusEndpoint.processGetRequest(""));
                 }
             );
         }
